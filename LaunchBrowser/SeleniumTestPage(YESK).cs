31bec50d0448dfdba830b246f02ef9d6f7e04810
WIN-QSEQ42B8HEV\ARCHON <kliucharovvlad@gmail.com>
2021-08-03 20:40:03 +0300

    New version

--- LaunchBrowser/SeleniumTestPage(YESK).cs
+++ LaunchBrowser/SeleniumTestPage(YESK).cs
@@ -1,31 +1,34 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using System.Collections.Generic;
 
 namespace LaunchBrowser
 {
     [TestFixture]
+
     class YeskSite
     {
         [Test]
         public void YeskTasks()
         {
+
+            stirng queryBa = 'QA Engineer';
+
             IWebDriver driver = new ChromeDriver();
             driver.Manage().Window.Maximize();
             driver.Navigate().GoToUrl("http://yesk.com.ua/webdriver-csharp/");
-            IWebElement searchElement = driver.FindElement(By.XPath("//*[contains(text(),'Поиск элементов на странице')]"));
-            searchElement.Click();
-            IWebElement goToArticle = driver.FindElement(By.XPath("//*[contains(text(),'CONTROLS PAGE')]"));
-            goToArticle.Click();
-            IWebElement selectButtonQA = driver.FindElement(By.Name("qa"));
-            selectButtonQA.Click();
-            IWebElement selectButtonBA = driver.FindElement(By.Name("ba"));
-            selectButtonBA.Click();
-            IWebElement selectRadioLess1 = driver.FindElement(By.Id("lessone"));
-            selectRadioLess1.Click();
-            IWebElement selectMore5 = driver.FindElement(By.Id("morefive"));
-            selectMore5.Click();
+
+            IWebElement searchElement = driver.FindElement(By.XPath("//*[contains(text(),'Поиск элементов на странице')]")).Click();
+
+            IWebElement goToArticle = driver.FindElement(By.XPath("//*[contains(text(),'CONTROLS PAGE')]")).Click();
+
+            IWebElement selectButtonQA = driver.FindElement(By.Name("qa")).Click();
+
+            IWebElement selectButtonBA = driver.FindElement(By.Name("ba")).Click();
+
+            IWebElement selectRadioLess1 = driver.FindElement(By.Id("lessone")).Click();
+
+            IWebElement selectMore5 = driver.FindElement(By.Id("morefive")).Click();
         }
     }
 }
